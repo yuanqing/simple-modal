b4746ca867a4e864b5cdb353ef86322a0561441a
yuanqing <desk@yuanqing.sg>
2015-07-25 13:33:17 +0800

    Use `elem` instead of `modal`

--- tiny-modal.js
+++ tiny-modal.js
@@ -11,12 +11,12 @@
     }
   }
 
-  function TinyModal(modal, opts) {
+  function TinyModal(elem, opts) {
 
     // Allow `TinyModal` to be called without the `new` keyword.
     var self = this;
     if (!(self instanceof TinyModal)) {
-      return new TinyModal(modal, opts);
+      return new TinyModal(elem, opts);
     }
 
     // Parse `opts`.
@@ -25,10 +25,10 @@
     self.onShow = opts.onShow || function() {};
     self.onHide = opts.onHide || function() {};
     self.isShown = false;
-    self.modal = modal;
+    self.elem = elem;
 
-    // Set default styles on the `modal`.
-    modal.style.cssText += 'display:none;position:fixed;top:0;right:0;bottom:0;left:0;overflow:auto;will-change:transform;';
+    // Set default styles on the `elem`.
+    elem.style.cssText += 'display:none;position:fixed;top:0;right:0;bottom:0;left:0;overflow:auto;will-change:transform;';
 
     // Bind the `close` and `open` methods to the click event of elements that
     // match `opts.closeSelector` and `opts.openSelector`.
@@ -39,11 +39,11 @@
       self.show(e.target);
     });
 
-    // Bind the `close` method to the `click` event on the `modal`. Only close
-    // the modal if we had clicked directly on the `modal` (ie. the overlay).
-    modal.addEventListener('click', function(e) {
-      if (e.target === modal) {
-        self.hide(modal);
+    // Bind the `close` method to the `click` event on the `elem`. Only close
+    // the modal if we had clicked directly on the `elem` (ie. the overlay).
+    elem.addEventListener('click', function(e) {
+      if (e.target === elem) {
+        self.hide(elem);
       }
     });
 
@@ -57,29 +57,29 @@
 
   }
 
-  TinyModal.prototype.show = function(target) {
+  TinyModal.prototype.show = function(triggerElem) {
     var self = this;
     if (!self.isShown) {
       // Disable scrolling on the window, and show the `modal`.
       document.body.style.overflow = 'hidden';
-      self.modal.style.display = 'block';
+      self.elem.style.display = 'block';
       if (self.scrollTop) {
         // Scroll to the top of the modal.
-        self.modal.scrollTop = 0;
+        self.elem.scrollTop = 0;
       }
       self.isShown = true;
-      self.onShow(target, self.modal);
+      self.onShow(triggerElem, self.elem);
     }
   };
 
-  TinyModal.prototype.hide = function(target) {
+  TinyModal.prototype.hide = function(triggerElem) {
     var self = this;
     if (self.isShown) {
-      // Enable scrolling on the window, and hide the `modal`.
+      // Enable scrolling on the window, and hide the `elem`.
       document.body.style.overflow = '';
-      self.modal.style.display = 'none';
+      self.elem.style.display = 'none';
       self.isShown = false;
-      self.onHide(target, self.modal);
+      self.onHide(triggerElem, self.elem);
     }
   };
 
